07394c0f739db797fb19792b457cb73b88e7611a
sokhorn <sokhornkhy88@gmail.com>
2026-06-18 14:29:01 +0700

    chore: init expend note

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 android {
     namespace = "com.nextgen.expend"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
--- app/src/main/java/com/nextgen/expend/MainActivity.kt
+++ app/src/main/java/com/nextgen/expend/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.nextgen.expend
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.nextgen.expend.dialog.QuickExpenseTopDialog
 import com.nextgen.expend.ui.theme.NexExpendTheme
+import androidx.core.net.toUri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NexExpendTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    BakingScreen()
-                }
-            }
+        if (!Settings.canDrawOverlays(this)) {
+            startActivity(
+                Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    "package:$packageName".toUri()
+                )
+            )
+            finish()
+            return
         }
+
+        startService(Intent(this, QuickExpenseOverlayService::class.java))
+        finish()
+        overridePendingTransition(0, 0)
     }
 }
